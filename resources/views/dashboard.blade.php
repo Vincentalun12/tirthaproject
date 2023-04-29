199850b4220ad7577dda6272768d136863e5f422
Alun <vengalolyeah@gmail.com>
2023-04-29 20:52:45 +0700

    new

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,42 +9,62 @@
         </div>
     </div>-->
     <div class="py-10 pt-24">
-        <div class="grid grid-rows-1 grid-flow-col gap-1 max-w-7xl mx-auto p-1">
-            <div class="col-span-1 row-span-1 border rounded-full bg-slate-300 p-3">
-                <button class="bg-birubut text-white font-bold py-2 px-7 rounded-full">
-                    Info
-                  </button>
-            </div>
+        <div class="grid grid-rows-1 gap-1 max-w-7xl mx-auto p-1">
+          <div class="col-span-1 row-span-1 border rounded-full bg-slate-300 p-3">
+            <button class="text-white bg-birubut hover:bg-black focus:ring-4  focus:ring-slate-100 font-bold py-2 px-7 rounded-full">
+            Info
+            </button>
+          </div>
         </div>
-        <div class="grid grid-rows-3 md:grid-rows-4 grid-flow-col gap-2 max-w-7xl mx-auto p-1">
-            <div class="md:col-span-2 md:row-span-4 border-4 rounded-3xl bg-slate-100 border-birubut p-7 flex flex-col justify-center items-center">
-                <h1 class="text-5xl font-poppins text-center font-black">Butuh Bantuan</h1>
-                <p class="text-center px-10 py-4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure</p>
-                    <button class="bg-birubut text-white font-bold py-2 px-24 rounded-full">
-                        Chat
-                      </button>
+        <div class="px-6 py-8 md:px-14">
+          <div class="container mx-auto max-w-7xl">
+            <div class="grid lg:grid-cols-2 gap-12">
+              <div>
+                <div class="">
+                    <div id="default-carousel" class="relative w-full" data-carousel="slide">
+                        <div class="relative overflow-hidden rounded-lg h-[300px] md:h-[485px]">
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="{{asset('image/hanyacoba.png')}}" class="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="{{asset('image/hanyacoba.png')}}" class="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                                <img src="{{asset('image/hanyacoba.png')}}" class="absolute block w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                            </div>
+                        </div>
+                        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                            <button type="button" class="w-3 h-3 rounded-full bg-black hover:bg-birubut" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                            <button type="button" class="w-3 h-3 rounded-full bg-black hover:bg-birubut" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                            <button type="button" class="w-3 h-3 rounded-full bg-black hover:bg-birubut" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                        </div>
+                    </div> 
+                </div>
+              </div>
+              <div>
+                <div class="border-4 rounded-3xl bg-slate-100 border-birubut p-7 lg:bg-[url('/public/image/cart1.png')]">
+                  <h1 class="text-5xl font-poppins text-left font-black">Order</h1>
+                  <p class="text-left font-poppins pt-3 sm:pr-32">Order jasa laundry dari kami untuk memudahkan pekerjaan anda</p>
+                  <button class="text-white bg-birubut hover:bg-black focus:ring-4  focus:ring-slate-100 font-bold py-2 px-12 mt-3 rounded-full">
+                  Order
+                  </button>
+                </div>
+                <div class="border-4 rounded-3xl bg-slate-100 border-birubut p-7 my-8 md:my-10 lg:bg-[url('/public/image/delivery1.png')]">
+                  <h1 class="text-5xl font-poppins text-left font-black">Tracking</h1>
+                  <p class="text-left font-poppins pt-3 sm:pr-48">Lacak pesanan anda disini dengan mudah dan nyaman.
+                  </p>
+                  <button class="text-white bg-birubut hover:bg-black focus:ring-4  focus:ring-slate-100 font-bold py-2 px-10 mt-3 rounded-full">
+                  Tracking
+                  </button>
+                </div>
+              </div>
             </div>
-            <div class="md:col-span-2 md:row-span-2 border-4 rounded-3xl bg-slate-100 border-birubut p-7 bg-[url('/public/image/cart1.png')]">
-                <h1 class="text-5xl font-poppins text-left font-black">Order</h1>
-                <p class="text-left ">Lakukan pemesanan sekarang juga.</p>
-                    <button class="bg-birubut text-white font-bold py-2 px-12 mt-3 rounded-full">
-                        Order
-                      </button>
-            </div>
-            <div class="md:col-span-2 md:row-span-2 col-span-1 border-4 rounded-3xl bg-slate-100 border-birubut p-7">
-                <h1 class="text-5xl font-poppins text-left font-black">Tracking</h1>
-                <p class="text-left ">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure</p>
-                    <button class="bg-birubut text-white font-bold py-2 px-10 mt-3 rounded-full">
-                        Tracking
-                      </button>
-            </div>
+          </div>
         </div>
     </div>
-    <footer class="p-5 bg-birutext shadow py-4 md:px-16 md:py-5">
-        <div class="text-center">
-          <p class="inline-block text-white font-poppins md:text-[16px] text-[10px]">© 2023<span class="block relative bottom-0">Website By Aseprite</span></p>
-        </div>
-      </footer>
+        <footer class="p-5 bg-birutext shadow py-4 md:px-16 md:py-5">
+            <div class="text-center">
+              <p class="inline-block text-white font-poppins md:text-[16px] text-[10px]">© 2023<span class="block relative bottom-0">Website By Aseprite</span></p>
+            </div>
+          </footer>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow-md fixed w-full">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow-md fixed w-full z-50">
     <!-- Primary Navigation Menu -->
     <div class="max-w-full mx-auto px-4 md:px-24 py-2">
-        <div class="flex justify-between h-16">
+        <div class="flex flex-wrap md:container items-center justify-between md:mx-auto h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center px-2">
@@ -9,17 +9,22 @@
                         <x-logo-tirtha class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-md leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-800 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
