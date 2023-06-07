3464e9d76b2ce094ee26b2c19691af771ff309dd
Alun <vengalolyeah@gmail.com>
2023-06-07 21:10:49 +0700

    new

--- resources/views/tracking.blade.php
+++ resources/views/tracking.blade.php
@@ -7,17 +7,17 @@
                  <span class="text-lg font-poppins font-bold">Riwayat Pelacakan</span>
               </div>
               <x-tracking.berhasil>
-                <x-slot name="no_pelacakan">1274 1212</x-slot>
-                <x-slot name="tanggal">4/3/2023</x-slot>
-             </x-tracking.berhasil>
+                 <x-slot name="no_pelacakan">1274 1212</x-slot>
+                 <x-slot name="tanggal">4/3/2023</x-slot>
+              </x-tracking.berhasil>
               <x-tracking.proses>
                  <x-slot name="no_pelacakan">1274 1212</x-slot>
                  <x-slot name="tanggal">4/3/2023</x-slot>
               </x-tracking.proses>
               <x-tracking.batal>
-                <x-slot name="no_pelacakan">1274 1212</x-slot>
-                <x-slot name="tanggal">4/3/2023</x-slot>
-             </x-tracking.batal>
+                 <x-slot name="no_pelacakan">1274 1212</x-slot>
+                 <x-slot name="tanggal">4/3/2023</x-slot>
+              </x-tracking.batal>
               <a href="" class="flex justify-center pt-4">
               <span class="text-sm font-poppins font-semi text-gray-400 hover:text-black">Load more</span>
               </a>
